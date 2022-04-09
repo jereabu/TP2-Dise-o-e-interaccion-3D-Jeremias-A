7a333fe4df4060aa8b2ed68aa1c5b3f7539a1f26
jereabu <jeremiasabulafia@gmail.com>
2022-04-09 15:02:23 -0300

    Finished btn and panel

--- Assets/validarContrasena.cs
+++ Assets/validarContrasena.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class validarContrasena : MonoBehaviour
 {
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
+    
     // Start is called before the first frame update
     void Start()
     {
+        contraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
         
     }
 
@@ -16,5 +25,19 @@ public class validarContrasena : MonoBehaviour
         
     }
     public void validarContraseña()
-    { }
+    {
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña Incorrecta";
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
 }
